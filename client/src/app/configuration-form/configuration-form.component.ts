b08b270e28b3a7d3c5cb8ba23c58789d5418a2a8
nethbotheju <131664638+nethbotheju@users.noreply.github.com>
2024-11-16 19:13:04 +0530

    add configaration form in frontend

--- client/src/app/configuration-form/configuration-form.component.ts
+++ client/src/app/configuration-form/configuration-form.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-configuration-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './configuration-form.component.html',
   styleUrl: './configuration-form.component.css',
 })
 export class ConfigurationFormComponent {
+  numOfCustomers: number = 10; // Default value
+  customers: { id: number; priority: string }[] = [];
+
+  updateCustomers() {
+    const newCustomers = [];
+    for (let i = 1; i <= this.numOfCustomers; i++) {
+      const existing = this.customers.find((cust) => cust.id === i);
+      newCustomers.push({
+        id: i,
+        priority: '3',
+      });
+    }
+    this.customers = newCustomers;
+  }
+
+  // Submit data
+  submit() {
+    console.log(this.customers);
+  }
+
   constructor(private router: Router) {}
 
   navigateToHome() {
